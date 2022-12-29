7b802517a248ff6f6738bebb3292e9e94595545d
Kamil Chwesiuk <kamil.chwesiuk@gmail.com>
2022-12-29 16:25:54 +0100

    adding target to value in calculateButtonPercent and adding reset function

--- homework-6/tip-calculator/jscript.js
+++ homework-6/tip-calculator/jscript.js
@@ -1,15 +1,15 @@
 ////////--------------RIGHT CONTAINER------------------/////
 const tipPrice = document.querySelector(".tip-price");
 const totalPrice = document.querySelector(".total-price");
+let resetBill = document.querySelector(".reset-btn");
 ////////--------------LEFT CONTAINER------------------/////
 let billBtn = document.querySelector(".bill-input");
 let numbOfPeople = document.querySelector(".person-input");
 let customPercent = document.querySelector(".percentage-custom-block");
-///-------------------------SPLITING--------------------//////
-let cantZero = document.querySelector(".cantZero");
-let resetBill = document.querySelector(".reset-btn");
 let buttonPercent = document.querySelectorAll(".percentage-block");
-
+let cantZero = document.querySelector(".cantZero");
+let formReset = document.querySelector("form");
+///-------------------------SPLITING--------------------//////
 function calcCustomPercent() {
   if (numbOfPeople.value == "") {
     cantZero.style.color = "red";
@@ -34,69 +34,48 @@ function calcCustomPercent() {
   }
 }
 
-function calcButtonPercents() {
-  buttonPercent.forEach((percents) => {
-    if (numbOfPeople.value == "") {
-      cantZero.style.color = "red";
-      numbOfPeople.style.borderColor = "red";
-    } else if (percents.value !== "") {
-      let calcPercent = parseInt(percents.value);
-      const tipPerPerson = (calcPercent * parseInt(billBtn.value)) / 100 / parseInt(numbOfPeople.value);
-      let totalPricePersonOne = parseInt(billBtn.value) / parseInt(numbOfPeople.value) + tipPerPerson;
-      tipPrice.innerHTML = "$" + tipPerPerson.toFixed(2);
-      totalPrice.innerHTML = "$" + totalPricePersonOne.toFixed(2);
-      cantZero.style.color = "transparent";
-      numbOfPeople.style.borderColor = "transparent";
-      tipPrice.style.color = "hsl(172, 87%, 79%)";
-    } else {
-      const tipAmount = billBtn / parseInt(numbOfPeople.value).toFixed(2);
-      totalPrice.innerHTML = "$" + tipAmount.toFixed(2);
-      cantZero.style.color = "transparent";
-      tipPrice.style.color = "hsl(172, 87%, 79%)";
-      numbOfPeople.style.borderColor = "hsl(172, 67%, 45%) solid thin";
-    }
-  });
+function calcButtonPercents(percents) {
+  if (numbOfPeople.value == "") {
+    cantZero.style.color = "red";
+    numbOfPeople.style.borderColor = "red";
+  } else if (percents.target.value !== "") {
+    let calcPercent = parseInt(percents.target.value);
+    const tipPerPerson = (calcPercent * parseInt(billBtn.value)) / 100 / parseInt(numbOfPeople.value);
+    let totalPricePersonOne = parseInt(billBtn.value) / parseInt(numbOfPeople.value) + tipPerPerson;
+    tipPrice.innerHTML = "$" + tipPerPerson.toFixed(2);
+    totalPrice.innerHTML = "$" + totalPricePersonOne.toFixed(2);
+    cantZero.style.color = "transparent";
+    numbOfPeople.style.borderColor = "transparent";
+    tipPrice.style.color = "hsl(172, 87%, 79%)";
+  } else {
+    const tipAmount = billBtn / parseInt(numbOfPeople.value).toFixed(2);
+    totalPrice.innerHTML = "$" + tipAmount.toFixed(2);
+    cantZero.style.color = "transparent";
+    tipPrice.style.color = "hsl(172, 87%, 79%)";
+    numbOfPeople.style.borderColor = "hsl(172, 67%, 45%) solid thin";
+  }
 }
-buttonPercent.forEach((buttonClick) => {
-  buttonClick.addEventListener("click", calcButtonPercents);
-});
-// splittingPercent.forEach((percentage) => {
-//   percentage.addEventListener("click", () => {
-//     if (numbOfPeople.value == "") {
-//       cantZero.style.color = "red";
-//       numbOfPeople.style.borderColor = "red";
-//     } else if (percentage.value !== "") {
-//       let calcPercent = parseInt(percentage.value);
-//       const tipPerPerson = (calcPercent * parseInt(billBtn.value)) / 100 / parseInt(numbOfPeople.value);
-//       let totalPricePersonOne = parseInt(billBtn.value) / parseInt(numbOfPeople.value) + tipPerPerson;
-//       tipPrice.innerHTML = "$" + tipPerPerson.toFixed(2);
-//       totalPrice.innerHTML = "$" + totalPricePersonOne.toFixed(2);
-//       cantZero.style.color = "transparent";
-//       numbOfPeople.style.borderColor = "transparent";
-//       tipPrice.style.color = "hsl(172, 87%, 79%)";
-//     } else {
-//       const tipAmount = billBtn / parseInt(numbOfPeople.value).toFixed(2);
-//       totalPrice.innerHTML = "$" + tipAmount.toFixed(2);
-//       cantZero.style.color = "transparent";
-//       tipPrice.style.color = "hsl(172, 87%, 79%)";
-//       numbOfPeople.style.borderColor = "hsl(172, 67%, 45%) solid thin";
-//     }
-//   });
-// });
 ////////////-----------RESET BUTTON------------///////////////////////
-resetBill.addEventListener("click", () => {
-  document.querySelector("form").reset();
+function resetAll() {
+  formReset.reset();
   cantZero.style.color = "transparent";
   numbOfPeople.style.borderColor = "transparent";
   tipPrice.style.color = "hsl(172, 67%, 45%)";
   resetBill.style.background = "hsl(186, 14%, 43%)";
   tipPrice.innerHTML = "$0.00";
   totalPrice.innerHTML = "$0.00";
-});
+}
 ////////////------------------ADD EVENT LISTENER--------------/////////////
-
 addEventListener("change", (prevent) => {
   resetBill.style.background = "hsl(172, 67%, 45%)";
   prevent.preventDefault();
   calcCustomPercent();
+});
+
+buttonPercent.forEach((buttonClick) => {
+  buttonClick.addEventListener("click", calcButtonPercents);
+});
+
+resetBill.addEventListener("click", () => {
+  resetAll();
 });
